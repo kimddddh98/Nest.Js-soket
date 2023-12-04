f83c99b60c185be7a99775be2ca40c7def3f13b0
kimddddh98 <amawang11@gmail.com>
2023-12-04 23:54:04 +0900

    로직 컨트롤러에서 서비스로 옮겨 작성

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -3,7 +3,6 @@ import {
   Controller,
   Delete,
   Get,
-  NotFoundException,
   Param,
   Patch,
   Post
@@ -19,66 +18,28 @@ interface PostModel {
   commentCount: number
 }
 
-let posts: PostModel[] = [
-  {
-    id: 1,
-    author: 'official',
-    title: '제목',
-    content: '컨텐츠',
-    likeCount: 0,
-    commentCount: 0
-  },
-  {
-    id: 2,
-    author: 'official',
-    title: '2번째',
-    content: '오피셜 2번째',
-    likeCount: 9999,
-    commentCount: 9999
-  },
-  {
-    id: 3,
-    author: 'official',
-    title: '3번째',
-    content: '오피셜 3번째',
-    likeCount: 9999,
-    commentCount: 9999
-  }
-]
-
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
   @Get()
-  getPosts(): PostModel[] {
-    return posts
+  getPosts() {
+    return this.postsService.getAllPosts()
   }
 
   @Get(':id')
   getPost(@Param('id') id: string): PostModel {
-    const post = posts.find(post => post.id === +id)
-    if (!post) {
-      throw new NotFoundException()
-    }
-    return post
+    return this.postsService.getPostById(+id)
   }
+
   @Post()
   postPost(
     @Body('author') author: string,
     @Body('title') title: string,
     @Body('content') content: string
   ) {
-    const post: PostModel = {
-      id: posts[posts.length - 1].id + 1,
-      author,
-      title,
-      content,
-      likeCount: 0,
-      commentCount: 0
-    }
-    posts = [...posts, post]
-    return post
+    return this.postsService.createPost(author, title, content)
   }
+
   @Patch(':id')
   patchPost(
     @Param('id') id: string,
@@ -86,29 +47,10 @@ export class PostsController {
     @Body('title') title?: string,
     @Body('content') content?: string
   ) {
-    const post = posts.find(post => post.id === +id)
-    if (!post) {
-      throw new NotFoundException()
-    }
-    if (author) {
-      post.author = author
-    }
-    if (title) {
-      post.title = title
-    }
-    if (post.content) {
-      post.content = content
-    }
-    posts = posts.map(prePost => (prePost.id === +id ? post : prePost))
-    return post
+    return this.postsService.updatePost(+id, author, title, content)
   }
   @Delete(':id')
   deletePost(@Param('id') id: string) {
-    const post = posts.find(post => post.id === +id)
-    if (!post) {
-      throw new NotFoundException()
-    }
-    posts = posts.filter(post => post.id !== +id)
-    return id
+    return this.postsService.deletePost(+id)
   }
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,90 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, NotFoundException } from '@nestjs/common'
 
+export interface PostModel {
+  id: number
+  author: string
+  title: string
+  content: string
+  likeCount: number
+  commentCount: number
+}
+
+let posts: PostModel[] = [
+  {
+    id: 1,
+    author: 'official',
+    title: '제목',
+    content: '컨텐츠',
+    likeCount: 0,
+    commentCount: 0
+  },
+  {
+    id: 2,
+    author: 'official',
+    title: '2번째',
+    content: '오피셜 2번째',
+    likeCount: 9999,
+    commentCount: 9999
+  },
+  {
+    id: 3,
+    author: 'official',
+    title: '3번째',
+    content: '오피셜 3번째',
+    likeCount: 9999,
+    commentCount: 9999
+  }
+]
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  getAllPosts() {
+    return posts
+  }
+  getPostById(id: number) {
+    const post = posts.find(post => post.id === +id)
+    if (!post) {
+      throw new NotFoundException()
+    }
+    return post
+  }
+
+  createPost(author: string, title: string, content: string) {
+    const post: PostModel = {
+      id: posts[posts.length - 1].id + 1,
+      author,
+      title,
+      content,
+      likeCount: 0,
+      commentCount: 0
+    }
+    posts = [...posts, post]
+    return post
+  }
+
+  updatePost(id: number, author?: string, title?: string, content?: string) {
+    const post = posts.find(post => post.id === id)
+    if (!post) {
+      throw new NotFoundException()
+    }
+    if (author) {
+      post.author = author
+    }
+    if (title) {
+      post.title = title
+    }
+    if (post.content) {
+      post.content = content
+    }
+    posts = posts.map(prePost => (prePost.id === id ? post : prePost))
+    return post
+  }
+
+  deletePost(id: number) {
+    const post = posts.find(post => post.id === id)
+    if (!post) {
+      throw new NotFoundException()
+    }
+    posts = posts.filter(post => post.id !== id)
+    return id
+  }
+}
