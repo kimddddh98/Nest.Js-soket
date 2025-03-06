fee0cc59bb60c584130a952cea28417e826208a9
kimddddh98 <amawang11@gmail.com>
2025-03-07 00:48:57 +0900

    feat : 특정 방과 소켓 통신

--- src/chats/chat.gateway.ts
+++ src/chats/chat.gateway.ts
@@ -1,10 +1,12 @@
 import {
+  ConnectedSocket,
   MessageBody,
   OnGatewayConnection,
   SubscribeMessage,
   WebSocketGateway,
+  WebSocketServer
 } from '@nestjs/websockets'
-import { Socket } from "socket.io"
+import { Server, Socket } from 'socket.io'
 
 @WebSocketGateway({
   /**
@@ -15,6 +17,9 @@ import { Socket } from "socket.io"
   namespace: 'chats'
 })
 export class ChatGateway implements OnGatewayConnection {
+  @WebSocketServer()
+  server: Server
+
   handleConnection(soket: Socket) {
     console.log('connection', soket.id)
     return soket.id
@@ -28,7 +33,28 @@ export class ChatGateway implements OnGatewayConnection {
    *
    */
   @SubscribeMessage('send_message')
-  sendMessage(@MessageBody() data: string) {
+  sendMessage(
+    @MessageBody() data: { roomId: number; message: string },
+    @ConnectedSocket() soket: Socket
+  ) {
     console.log(data)
+
+    /**
+     * server.in 메서드를 사용하여 방에 속한 모든 클라이언트에게 메시지를 보낼 수 있음
+     * this.server.in(data.roomId.toString()).emit('receive_message', data.message)
+     */
+
+    /**
+     * soket.to 메서드를 사용하여 자신을 제외한 방에 속한 모든 클라이언트에게 메시지를 보낼 수 있음
+     */
+    soket.to(data.roomId.toString()).emit('receive_message', data.message)
+  }
+
+  @SubscribeMessage('enter_room')
+  enterRoom(@MessageBody() data: number[], @ConnectedSocket() soket: Socket) {
+    for (const roomId of data) {
+      soket.join(roomId.toString())
+      console.log('join', roomId)
+    }
   }
 }
